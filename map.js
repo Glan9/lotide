2031adf5d8bb3af68dd8351fa0e18c514073bf1c
Glan9 <splcurran@hotmail.com>
2021-11-18 21:20:12 +0000

    Added map.js

---
+++ map.js
@@ -0,0 +1,35 @@
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const eqArrays = function(list1, list2) {
+  if (list1.length !== list2.length) {
+    return false;
+  }
+
+  for (let i=0; i<list1.length; i++) {
+    if (list1[i] !== list2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+assertArraysEqual(map([], (x) => x * 2), [])
