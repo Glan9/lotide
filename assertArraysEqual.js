3768bac870d04b22c4f3bd9897a3edbf2d384ebd
Glan9 <splcurran@hotmail.com>
2021-11-22 02:35:33 +0000

    Refactoring assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(list1, list2) {
-  if (list1.length !== list2.length) {
-    return false;
-  }
-
-  for (let i=0; i<list1.length; i++) {
-    if (list1[i] !== list2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -20,5 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+module.exports = assertArraysEqual;
