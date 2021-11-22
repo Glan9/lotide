4facd8c6795a6f457a03dff4a0077b6acd1566ec
Glan9 <splcurran@hotmail.com>
2021-11-22 02:38:06 +0000

    Refactoring middle.js

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(list1, list2) {
-  if (list1.length !== list2.length) {
-    return false;
-  }
-
-  for (let i=0; i<list1.length; i++) {
-    if (list1[i] !== list2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const middle = function(array){
   if (array.length <= 2) { // Too short array
     return [];
@@ -32,8 +10,4 @@ const middle = function(array){
   }
 };
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+module.exports = middle;
