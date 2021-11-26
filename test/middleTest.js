c02af90448456051a5656b8a3cffd1ed885f713b
Glan9 <splcurran@hotmail.com>
2021-11-26 23:27:05 +0000

    Refactor middle tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 const middle = require('../middle.js');
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+  it("should return an empty array when passed an empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it("should return an empty array when passed an array with 1 element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("should return an empty array when passed an array with 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("should return 1 element when the input has odd length", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("should return 2 elements when the input has even length", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
